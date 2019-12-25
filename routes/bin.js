fffe23aa3d9ba80b770d5ef0fdac02b35a9ea26b
sankhadip samanta <sankhsamanta.cse17@chitkarauniversity.edu.in>
2019-12-25 17:48:11 +0530

    update

--- routes/bin.js
+++ routes/bin.js
@@ -2,7 +2,7 @@ const express=require('express');
 const router=express.Router();
 const checkToken=require('../jwt/jwt').checkToken;
 const connection=require('../db/db');
-
+const fs=require('fs');
 
 router.delete('/restore/:binid/:filetype',checkToken, function(request, response)
 {
@@ -48,22 +48,71 @@ router.delete('/delete/:binid',checkToken, function(request, response)
 {
     const {email}=request.decoded;
     const { binid }=request.params;
+    var getFileNameFromQuery="select * from bin where binid = ?";
     var deleteFromBinQuery="delete from bin where binid = ?";
-    connection.query(deleteFromBinQuery,[binid], function(err, result)
+    connection.query(getFileNameFromQuery,[binid], function(err, result)
     {
         if(err)
         {
             console.log(err);
-            response.status(400).send({
-                code:400,
+            response.status(500).send({
+                code:500,
                 message:"Internal error"
             })
         }
         else
         {
-            response.send({
-                code:200,
-                message:"Deleted"
+            console.log(result);
+            if(result[0].filetype==='image' || result[0].filetype==='audio')
+            {
+                fs.unlink(`../gravity/upload/${result[0].filetype}/${result[0].name}`, (err)=>
+                {
+                    if(err)
+                    {
+                        console.log(err);
+                    }
+                    console.log('file deleted forever');
+                })
+            }
+            else if(result[0].filetype==='doc')
+            {
+                fs.unlink(`../gravity/upload/docs/${result[0].name}`, (err)=>
+                {
+                    if(err)
+                    {
+                        console.log(err);
+                    }
+                    console.log('Docs file deleted forever');
+                })
+            }
+            else if(result[0].filetype==='video')
+            {
+                fs.unlink(`../gravity/videos/watchvideo/${result[0].name}`,(err)=>
+                {
+                    if(err)
+                    {
+                        console.log(err);
+                    }
+                    console.log('Video File deleted forever')
+                })
+            }
+            connection.query(deleteFromBinQuery,[binid], function(err, result1)
+            {
+                if(err)
+                {
+                    console.log(err);
+                    response.status(500).send({
+                        code:500,
+                        message:"Internal error"
+                    })
+                }
+                else
+                {
+                    response.send({
+                        code:200,
+                        message:"Deleted"
+                    })
+                }
             })
         }
     })
