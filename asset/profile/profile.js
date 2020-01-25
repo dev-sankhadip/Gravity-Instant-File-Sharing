5a1f8135a8d73592a63678202b48b51f4ef76c3a
sankhadip samanta <sankhsamanta.cse17@chitkarauniversity.edu.in>
2020-01-25 12:46:24 +0530

    code: set profile to img tag after upload

--- asset/profile/profile.js
+++ asset/profile/profile.js
@@ -1,4 +1,6 @@
 const image_update_btn=document.getElementById("image-update");
+const profileImage=document.getElementById("profile-image");
+
 image_update_btn.addEventListener("click", function(e)
 {
     const fileInput=document.createElement("input");
@@ -25,7 +27,7 @@ image_update_btn.addEventListener("click", function(e)
         convertToBase64(e.target.files[0])
         .then((res)=>
         {
-            console.log(res);
+            profileImage.src=res;
         })
         .catch((err)=>
         {
